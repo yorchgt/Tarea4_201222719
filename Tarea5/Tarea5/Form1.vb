4c78c24ed2cbf747bd9ab786e751145cf066e3b9
yorchgt <99776039+yorchgt@users.noreply.github.com>
2022-08-17 18:37:05 -0600

    Add files via upload

---
+++ Tarea5/Tarea5/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i, j, n As Integer
+
+
+        j = 1
+        n = TextBox1.Text
+        For i = 1 To n Step 1
+            j = j * i
+        Next
+        Label3.Text = j.ToString
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If MsgBox("Desea Salir?", vbQuestion + vbYesNo, "Pregunta") = vbYes Then
+            End
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label3.Text = ""
+        TextBox1.Text = ""
+    End Sub
+End Class
